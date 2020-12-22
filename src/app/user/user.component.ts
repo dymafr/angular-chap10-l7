2495f61e57af2026bf7565651ca526978e82397c
dymafr <erwan1309@hotmail.com>
2020-12-22 10:46:21 +0100

    end

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -16,5 +16,11 @@ export class UserComponent implements OnInit {
     this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
       this.id = paramMap.get("id");
     });
+    this.activatedRoute.queryParamMap.subscribe((paramMap: ParamMap) => {
+      this.name = paramMap.get("name");
+    });
+    this.activatedRoute.fragment.subscribe((fragment: string) => {
+      console.log(fragment);
+    });
   }
 }
