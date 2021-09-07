7d61f8b8cd227f084d3187cd712b5e34f6c3d809
dymafr <erwan1309@hotmail.com>
2021-09-07 14:36:28 +0200

    1 file modified

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, ParamMap } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
-  selector: "app-user",
-  templateUrl: "./user.component.html",
-  styleUrls: ["./user.component.scss"]
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-  public id: string;
-  public name: string;
+  public id?: string;
+  public name?: string;
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
-      this.id = paramMap.get("id");
+      this.id = paramMap.get('id')!;
     });
     this.activatedRoute.queryParamMap.subscribe((paramMap: ParamMap) => {
-      this.name = paramMap.get("name");
+      this.name = paramMap.get('name')!;
     });
-    this.activatedRoute.fragment.subscribe((fragment: string) => {
+    this.activatedRoute.fragment.subscribe((fragment: string | null) => {
       console.log(fragment);
     });
   }
